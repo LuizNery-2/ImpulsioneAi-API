c6c54a81dca9e7f959539215ffe30a6589ee38f6
Luiz Nery <Luiznery2004@gmail.com>
2024-03-14 19:11:39 -0300

    Edit Feat: Corrigindo bugs do DTO dos empreendedores

--- src/main/java/com/unit/impulsioneai/dtos/EmpreendedoresRecordDto.java
+++ src/main/java/com/unit/impulsioneai/dtos/EmpreendedoresRecordDto.java
@@ -8,7 +8,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record EmpreendedoresRecordDto(@NotBlank UUID idEmpreededor, @NotBlank String biografia, 
+public record EmpreendedoresRecordDto(@NotBlank String biografia,
                                     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")Date tempoAtuacao,
                                     @NotBlank String telefone, @NotBlank String endereco, @NotBlank String site, 
                                     @NotBlank String redeSocial, @NotBlank boolean verificado) {
