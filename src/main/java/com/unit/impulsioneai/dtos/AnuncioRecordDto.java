a6c702b9ef527c942bf41943876067ab277941fa
Luiz Nery <Luiznery2004@gmail.com>
2024-03-17 20:20:28 -0300

    feat: Criando o AnuncioRecordDto

---
+++ src/main/java/com/unit/impulsioneai/dtos/AnuncioRecordDto.java
@@ -0,0 +1,12 @@
+package com.unit.impulsioneai.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Date;
+
+public record AnuncioRecordDto(@NotBlank String urlAnuncio,
+                               @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy") Date dataInicialAnuncio,
+                               @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy") Date dataFinalAnuncio
+                               ) {
+}
