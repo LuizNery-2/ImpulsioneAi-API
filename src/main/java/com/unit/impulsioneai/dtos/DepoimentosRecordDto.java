68272937bdfa017b901bbf3c4145fe6f65620084
Jhonnatas Mendonça <jhonnatas.m2000@gmail.com>
2024-03-19 02:43:10 -0300

    Feat: Dto da classe de depoimentos desenvolvido.

---
+++ src/main/java/com/unit/impulsioneai/dtos/DepoimentosRecordDto.java
@@ -0,0 +1,8 @@
+package com.unit.impulsioneai.dtos;
+
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DepoimentosRecordDto(UUID idEmpreededor, @NotBlank String depoimento) {
+} 
