bd32b828a1193d810a347c3ca6c86ff9d9f88f5c
Jhonnatas Mendonça <jhonnatas.m2000@gmail.com>
2024-03-19 02:39:51 -0300

    Feat: Repository da classe de depoimentos desenvolvivdo.

---
+++ src/main/java/com/unit/impulsioneai/repositories/DepoimentosRepository.java
@@ -0,0 +1,10 @@
+package com.unit.impulsioneai.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unit.impulsioneai.models.DepoimentosModel;
+
+public interface DepoimentosRepository extends JpaRepository<DepoimentosModel, Integer>{
+
+    
+}
