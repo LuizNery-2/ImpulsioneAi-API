8eb96b24b23036fc5683b88619822642e1313b34
Jhonnatas Mendonça <jhonnatas.m2000@gmail.com>
2024-05-15 23:43:34 -0300

    Feat: função para atualizar biografia do empreendedor desenvolvida.

--- src/main/java/com/unit/impulsioneai/dtos/EmpreendedoresRecordDto.java
+++ src/main/java/com/unit/impulsioneai/dtos/EmpreendedoresRecordDto.java
@@ -11,5 +11,9 @@ public record EmpreendedoresRecordDto( int idNicho,
         ,String email, String planoAssinatura, String facebook, String instagram, String modalidade, int numeroVisitas
 
     ) {
+
+    public String getBiografia() {
+       return biografia;
+    }
     
 }
